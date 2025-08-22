dd0631bfe7c3c3383a3abf64b0a3c3e5cc68ff89
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2025-08-22 13:13:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-PICS = (os.environ.get("PICS", "")).split()
+API_ID = os.environ.get("API_ID", "8721997")
+API_HASH = os.environ.get("API_HASH", "47dbeccf2ade0e096f08dce7617f4f3b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8215877176:AAGxDk2r09IDBRBdBWixDQwTmjPNuwLliMM")
+ADMIN = int(os.environ.get("ADMIN", "1994878828 2002710801"))
+PICS = (os.environ.get("PICS", "https://envs.sh/Fkx.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
