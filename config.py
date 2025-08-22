c8eaba4ceb03986b5a6fbd33ea4912b0cf47a44f
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2025-08-22 13:15:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ADMIN = int(os.environ.get("ADMIN", "1994878828 2002710801"))
 PICS = (os.environ.get("PICS", "https://envs.sh/Fkx.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003027578148"))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "true").lower() == "true"  # Set "True" For accept new requests
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://deepakkadakkal30:RSE9t6lq0aBie3RK@cluster0.6quqrqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
